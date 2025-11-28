cffa20b03827af755580b36361af5a16a72f0ecd
victorien-hash <victorienfotso@gmail.com>
2025-11-28 14:01:01 -0500

    ajout du bouton pour retourner au tableau de bord a partir de nimporte quel page

--- src/components/UserBar.tsx
+++ src/components/UserBar.tsx
@@ -17,6 +17,9 @@ const UserBar = () => {
   return (
     <div className="user-info">
       <span>Bienvenue, {user?.username || "Utilisateur"} !</span>
+      <button onClick={() => navigate('/')} className="dashboard-button">
+        Tableau de Bord
+      </button>
       <button onClick={() => navigate("/profile")} className="profile-button">
         Mon profil
       </button>
